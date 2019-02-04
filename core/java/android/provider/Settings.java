bcd4a394b2858e8d01310f27793fd1b62beb7a53
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-04 21:26:22 -0200

    Merge tag 'android-9.0.0_r31' of https://android.googlesource.com/platform/frameworks/base into HEAD
    Android 9.0.0 Release 31 (PQ2A.190205.001)
    
    Change-Id: If3456cb132ad3e501186481430fd2ee25c8e6b58

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4590,7 +4590,8 @@ public final class Settings {
             OMNI_NAVIGATION_BAR_SHOW,
             OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT,
             OMNI_BOTTOM_GESTURE_SWIPE_LIMIT,
-            LOCK_QS_DISABLED
+            LOCK_QS_DISABLED,
+            NOTIFICATION_LIGHT_PULSE
         };
 
         /**
@@ -4812,7 +4813,7 @@ public final class Settings {
             VALIDATORS.put(DTMF_TONE_TYPE_WHEN_DIALING, DTMF_TONE_TYPE_WHEN_DIALING_VALIDATOR);
             VALIDATORS.put(HEARING_AID, HEARING_AID_VALIDATOR);
             VALIDATORS.put(TTY_MODE, TTY_MODE_VALIDATOR);
-            VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, NOTIFICATION_LIGHT_PULSE_VALIDATOR);
+            VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(POINTER_LOCATION, POINTER_LOCATION_VALIDATOR);
             VALIDATORS.put(SHOW_TOUCHES, SHOW_TOUCHES_VALIDATOR);
             VALIDATORS.put(WINDOW_ORIENTATION_LISTENER_LOG,
@@ -8719,6 +8720,8 @@ public final class Settings {
             VALIDATORS.put(HUSH_GESTURE_USED, HUSH_GESTURE_USED_VALIDATOR);
             VALIDATORS.put(MANUAL_RINGER_TOGGLE_COUNT, MANUAL_RINGER_TOGGLE_COUNT_VALIDATOR);
             VALIDATORS.put(LOCK_POWER_MENU_DISABLED, LOCK_POWER_MENU_DISABLED_VALIDATOR);
+            VALIDATORS.put(LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, BOOLEAN_VALIDATOR);
+            VALIDATORS.put(LOCK_SCREEN_SHOW_NOTIFICATIONS, BOOLEAN_VALIDATOR);
         }
 
         /**
@@ -13323,6 +13326,7 @@ public final class Settings {
          * Supported keys:
          * compatibility_wal_supported      (boolean)
          * wal_syncmode       (String)
+         * truncate_size      (int)
          *
          * @hide
          */
