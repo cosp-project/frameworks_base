1fe3431366e0929a5f5cfc566d7deef143ae2c8e
edi194 <misiakadrian@gmail.com>
2019-02-25 11:25:40 +0000

    Revert "CustomActions: Use settings provider to store value [2/3]"
    This reverts commit eca6bde729bc0f831211363fc6eda1c466adcfe5.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8510,20 +8510,6 @@ public final class Settings {
 
         
         /**
-         * Launch custom application when using the squeeze feature (active edge)
-         *
-         * @hide
-         */
-        public static final String SQUEEZE_CUSTOM_APP = "squeeze_custom_app";
-
-        /**
-         * Display friendly name of custom application launched when
-         * using the squeeze feature (active edge)
-         * @hide
-         */
-        public static final String SQUEEZE_CUSTOM_APP_FR_NAME = "squeeze_custom_app_fr_name";
-
-        /**
          * Enable face auto unlock on secure lock screens
          *
          * @hide
